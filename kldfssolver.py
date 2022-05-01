f1a2126089f1500898e226b742f299a291aa31d1
kkew3 <kps6326@hotmail.com>
2022-05-01 21:42:02 +0800

    Major adjustment: changed from Kernighan–Lin bisection to global Min-cut bisection. I also restructured the code so that it looks like fullsatsolver.py.

--- kldfssolver.py
+++ kldfssolver.py
@@ -3,14 +3,12 @@ DFS solver based on Kernighan-Lin partition algorithm to reduce recursion
 complexity.
 """
 
-import pdb
 import sys
 import itertools
 import collections
 import logging
 import functools
 import operator
-import contextlib
 
 import networkx as nx
 import numpy as np
@@ -19,33 +17,42 @@ import solverutils as sutils
 from solverutils import CID
 
 MAX_VARS = 32
-MAX_ITER = 2 ** 24  # takes approximately 1 second
+MAX_ITER = 2**24  # takes approximately 1 second
 
 NCKProblem = collections.namedtuple('NCKProblem', 'vars k')
 
 
 # a placeholder
-class NoSolutionError(Exception): pass
+class NoSolutionError(Exception):
+    pass
 
 
-def encode_board_as_nckproblems(board, mines_remain):
+def boxof(array, center, radius=1):
+    return array[max(0, center[0] - radius):center[0] + radius + 1,
+                 max(0, center[1] - radius):center[1] + radius + 1]
+
+
+def encode_board(board, mines_remain):
+    """
+    Encode the board into a list of ``NCKProblem``s and an optional
+    ``NCKProblem`` (mine hint). The problems may not be unique.
+    """
     vartable = np.arange(board.size).reshape(board.shape)
     problems = []
-    for x, y in zip(*np.where((board >= 1) & (board <= 8))):
-        box = board[max(0, x - 1):x + 2, max(0, y - 1):y + 2]
-        vbox = vartable[max(0, x - 1):x + 2, max(0, y - 1):y + 2]
-        if np.sum(box == CID['q']) > 0:
-            problems.append(NCKProblem(
-                vars=tuple(vbox[sx, sy] for sx, sy in
-                           zip(*np.where(box == CID['q']))),
-                k=(board[x, y] - np.sum(box == CID['m'])
-                   - np.sum(box == CID['f'])),
-            ))
-            assert problems[-1].k >= 0, problems[-1]
+    for x, y in zip(*np.nonzero((board >= 1) & (board <= 8))):
+        box = boxof(board, (x, y))
+        vbox = boxof(vartable, (x, y))
+        if np.any(box == CID['q']):
+            problems.append(
+                NCKProblem(
+                    tuple(vbox[box == CID['q']].tolist()),
+                    board[x, y]
+                    - np.sum((box == CID['m']) | (box == CID['f'])),
+                ))
     if mines_remain is not None:
-        mproblem = NCKProblem(vars=tuple(vartable[sx, sy] for sx, sy in
-                                         zip(*np.where(board == CID['q']))),
-                              k=mines_remain)
+        mproblem = NCKProblem(
+            tuple(vartable[board == CID['q']].tolist()), mines_remain)
+        assert mproblem.vars, (board, mproblem)
     else:
         mproblem = None
     return problems, mproblem
@@ -53,15 +60,15 @@ def encode_board_as_nckproblems(board, mines_remain):
 
 def reduce_problem(solutions, problem):
     new_vars = tuple(v for v in problem.vars if v not in solutions)
-    new_k = problem.k - sum(solutions[v] for v in problem.vars
-                            if v in solutions)
+    new_k = problem.k - sum(
+        solutions[v] for v in problem.vars if v in solutions)
     assert new_k >= 0, (solutions, problem)
     if not new_vars:
         return None
-    return NCKProblem(vars=new_vars, k=new_k)
+    return NCKProblem(new_vars, new_k)
 
 
-def simple_solve_attempt(problems, mproblem):
+def trivial_solve_attempt(problems, mproblem):
     unsolved_problems = []
     solutions = {}
     updated = True
@@ -77,8 +84,7 @@ def simple_solve_attempt(problems, mproblem):
                 updated = True
             else:
                 unsolved_problems.append(p)
-        problems = map(functools.partial(reduce_problem, solutions),
-                       unsolved_problems)
+        problems = (reduce_problem(solutions, p) for p in unsolved_problems)
         problems = set(filter(None, problems))
 
     if mproblem is not None:
@@ -94,10 +100,17 @@ def simple_solve_attempt(problems, mproblem):
     return solutions, confidences, problems, mproblem
 
 
-def make_problem_graph(problems, mproblem):
+def make_problem_graph(problems, mproblem, consider_mines_th: int = 5):
+    logger = logging.getLogger(__name__ + '.make_problem_graph')
     graph = nx.Graph()
     if mproblem is not None:
-        graph.add_node(mproblem, mr=True)
+        if len(mproblem.vars) <= MAX_VARS and mproblem.k <= consider_mines_th:
+            # mr=True is used to note that this is mine remaining problem
+            graph.add_node(mproblem, mr=True)
+        else:
+            logger.info(
+                'Not adding mproblem because it\'s intractable with '
+                '#vars=%d and #mines=%d', mproblem.vars, mproblem.k)
     for p in problems:
         # note that duplicate problems are removed automatically here
         graph.add_node(p, mr=False)
@@ -106,48 +119,90 @@ def make_problem_graph(problems, mproblem):
         vu = frozenset(u.vars)
         vv = frozenset(v.vars)
         jac = len(vu & vv) / len(vu | vv)
+        # jac > 0 means the joint set of vu and vv is nonempty
         if jac > 0:
+            # don't change the key name 'weight' -- it's used below in
+            # mincut_bisect
             graph.add_edge(u, v, weight=jac)
     return graph
 
 
-klbisect = nx.algorithms.community.kernighan_lin_bisection
+class NodeTooLessError(Exception):
+    pass
+
+
+def mincut_bisect(graph):
+    logger = logging.getLogger(__name__ + '.mincut_bisect')
+    if len(graph) < 2:
+        raise NodeTooLessError
+    logger.debug('Bisection start')
+    nodes = list(graph.nodes)
+    logger.debug('Nodes: %s', nodes)
+    u = nodes[0]
+    # Reference: https://stanford.edu/~rezab/classes/cme305/W14/Notes/4.pdf
+    results = [
+        nx.minimum_cut(graph, u, v, capacity='weight') for v in nodes[1:]
+    ]
+    mincut = min(results, key=lambda x: x[0])
+    logger.debug('Mincut result: %s', mincut)
+    return mincut[1]
+
+
+def guess(board: np.ndarray, all_blocs: np.ndarray, guess_edge_weight: float):
+    """
+    Random guess among all_blocs favoring edges by guess_edge_weight extent.
+
+    :param board: the board
+    :param all_blocs: of shape (m, 2) such that the ith row is the board
+           coordinate of an empty cell
+    :param guess_edge_weight: should be no less than 1.0
+    :return: the chosen board coordinate of shape (2,)
+    """
+    on_edge = ((all_blocs[:, 0] == 0)
+               | (all_blocs[:, 0] == board.shape[0] - 1)
+               | (all_blocs[:, 1] == 0)
+               | (all_blocs[:, 1] == board.shape[1] - 1))
+    weights = np.where(on_edge, guess_edge_weight, 1.0)
+    weights = weights / np.sum(weights)
+    rand_bloc = all_blocs[np.random.choice(
+        np.arange(all_blocs.shape[0]), p=weights)]
+    return rand_bloc
 
 
 def get_vars(problems):
     return functools.reduce(operator.or_, (set(p.vars) for p in problems))
 
 
-def solve_problems_graph(graph, max_vars, max_iter, solutions, confidences):
+def solve_problems_graph(graph, solutions, confidences) -> None:
+    """
+    :param graph: the problem graph
+    :param solutions: dict to put solutions in
+    :param confidences: dict to put confidences in
+    """
     logger = logging.getLogger(__name__ + '.solve_problems_graph')
-    to_remove = None
-    at_least_one_nonmproblem = False
-    for p, mr in dict(graph.nodes(data='mr')).items():
-        if mr and len(p.vars) > max_vars:
-            to_remove = p
-        if not mr:
-            at_least_one_nonmproblem = True
-            if to_remove is not None:
-                break
-    if to_remove and at_least_one_nonmproblem:
-        logger.warning('Removed mines_remain problem %s due to exceeding '
-                       'n_vars limit (%d > %d)',
-                       to_remove, len(to_remove.vars), max_vars)
-        graph.remove_node(to_remove)
-    for subgraph in map(graph.subgraph, nx.connected_components(graph)):
-        workingq = collections.deque([subgraph])
-        while workingq:
-            head = workingq.popleft()
-            n_vars = len(get_vars(head.nodes))
-            if n_vars > max_vars:
-                logger.warning('Performing Kernighan-Lin bisection due to '
-                               'exceeding n_vars limit (%d > %d)',
-                               n_vars, max_vars)
-                workingq.extend(map(graph.subgraph, klbisect(subgraph)))
-            else:
-                sols, confs = solve_problems(head.nodes, max_iter)
-                solutions.update(sols)
-                confidences.update(confs)
+    workingq = list(map(graph.subgraph, nx.connected_components(graph)))
+    while workingq:
+        top_graph = workingq.pop()
+        n_vars = len(get_vars(top_graph.nodes))
+        if n_vars > MAX_VARS:
+            logger.warning(
+                'Performing Min-cut bisection due to '
+                'exceeding n_vars limit (%d > %d)', n_vars, MAX_VARS)
+            # NodeTooLessError shouldn't be raised unless MAX_VARS is too small
+            workingq.extend(map(graph.subgraph, mincut_bisect(top_graph)))
+        else:
+            # top_graph.nodes, i.e. a set of problems.
+            # top_graph can't be an empty graph, as mincut_bisect won't output
+            # empty graph.
+            # there must be at least one node in top_graph
+            try:
+                sols, confs = dfs_solve_problems(top_graph.nodes)
+            except NoSolutionError:
+                logger.debug('NoSolutionError during dfs_solve_problems, with '
+                             'top_graph.nodes: %s; suppressed',
+                             top_graph.nodes)
+            solutions.update(sols)
+            confidences.update(confs)
 
 
 def _check_validity_trace(vars2problems, trace):
@@ -155,9 +210,8 @@ def _check_validity_trace(vars2problems, trace):
     Returns ``False`` if not valid; returns ``True`` if valid or not sure.
     """
     trace = dict(trace)
-    involved_problems = functools.reduce(operator.or_,
-                                         (set(vars2problems[v]) for v in
-                                          trace), set())
+    involved_problems = functools.reduce(operator.or_, (set(vars2problems[v])
+                                                        for v in trace), set())
     for p in involved_problems:
         try:
             if sum(trace[v] for v in p.vars) != p.k:
@@ -167,23 +221,29 @@ def _check_validity_trace(vars2problems, trace):
     return True
 
 
-def solve_problems(problems, max_iter):
-    logger = logging.getLogger(__name__ + '.solve_problems')
-    vars2problems = {v: [p for p in problems if v in p.vars]
-                     for v in get_vars(problems)}
+def dfs_solve_problems(problems):
+    """
+    Traverse all possibilities by deep first search and reach solutions.
+    """
+    logger = logging.getLogger(__name__ + '.dfs_solve_problems')
+    vars2problems = {
+        v: [p for p in problems if v in p.vars]
+        for v in get_vars(problems)
+    }
     varlist = sorted(vars2problems)
 
     candidate_solutions = []
-    to_open = collections.deque([[]])
+    to_open = [[]]
     n_iter = 0
     while to_open:
         top_trace = to_open.pop()
         # check if top_trace has already violated certain rules
         validity = _check_validity_trace(vars2problems, top_trace)
         n_iter += 1
-        if n_iter >= max_iter:
-            logger.warning('Break DFS iterations due to exceeding the '
-                           'upper limit %d', max_iter)
+        if n_iter >= MAX_ITER:
+            logger.warning(
+                'Break DFS iterations due to exceeding the '
+                'upper limit %d', MAX_ITER)
             break
         if not validity:
             continue
@@ -192,36 +252,87 @@ def solve_problems(problems, max_iter):
             to_open.append(top_trace + [(varlist[len(top_trace)], False)])
         else:
             candidate_solutions.append([x[1] for x in top_trace])
-    candidate_solutions = np.array(candidate_solutions)
-    probs = np.sum(candidate_solutions, axis=0) / candidate_solutions.shape[0]
-
-    # FIXME `candidate_solutions` is likely to be [], causing NaN here
-    solutions = dict(zip(varlist, map(bool, probs >= 0.5)))
-    confidence = dict(zip(varlist, np.maximum(probs, 1 - probs).tolist()))
+    if not candidate_solutions:
+        # this might happen when the DFS tree is very deep that exceeds
+        # MAX_ITER
+        raise NoSolutionError
+    # now candidate_solutions consists of 1 and -1, where 1 means there's mine
+    # and -1 means there's no mine
+    candidate_solutions = np.asarray(candidate_solutions) * 2 - 1
+    confidence = np.abs(np.sum(candidate_solutions, axis=0)) \
+            / candidate_solutions.shape[0]
+    confidence = dict(zip(varlist, confidence))
+    # if confidence == 0, presume there's no mine so that we can proceed
+    solutions = np.sign(np.sum(candidate_solutions, axis=0)) > 0
+    solutions = dict(zip(varlist, solutions))
     return solutions, confidence
 
 
-def solve(board, mines_remain):
-    logger = logging.getLogger(__name__ + '.solve')
-    problems, mproblem = encode_board_as_nckproblems(board, mines_remain)
-    if not any((problems, mproblem)) or np.all(board == CID['q']):
-        logger.info('Performing random guess due to lack to NCK constraints')
-        randbloc = np.unravel_index(np.random.randint(board.size), board.shape)
-        return np.concatenate((randbloc, [0]))[np.newaxis]
-
+def solve_board(board, mines_remain: int = None, consider_mines_th: int = 5):
+    logger = logging.getLogger(__name__ + '.solve_board')
+    problems, mproblem = encode_board(board, mines_remain)
+    logger.debug('Encoded board: %s; %s', problems, mproblem)
+    if not problems and not mproblem:
+        raise NoSolutionError
     solutions, confidences, problems, mproblem = \
-        simple_solve_attempt(problems, mproblem)
-    pgraph = make_problem_graph(problems, mproblem)
-    solve_problems_graph(pgraph, MAX_VARS, MAX_ITER, solutions, confidences)
+            trivial_solve_attempt(problems, mproblem)
+    logger.debug('Trivial solve complete with (partial) solutions: %s; '
+                 'confidences: %s', solutions, confidences)
+    logger.debug('(Possibly) reduced encoding: %s; %s', problems, mproblem)
+    pgraph = make_problem_graph(problems, mproblem, consider_mines_th)
+    if not pgraph and not solutions:
+        raise NoSolutionError
+    solve_problems_graph(pgraph, solutions, confidences)
+    logger.debug('Graph solve complete with solutions: %s; confidences: %s',
+                 solutions, confidences)
+    if not solutions:
+        raise NoSolutionError
     varlist = list(solutions)
     qidx = np.stack(np.unravel_index(varlist, board.shape), axis=1)
     mine = np.array([solutions[v] for v in varlist], dtype=np.int64)
     qidx_mine = np.concatenate((qidx, mine[:, np.newaxis]), axis=1)
     confidences = np.array([confidences[v] for v in varlist])
-    uscore = 1.0 - 1e-6
-    if np.max(confidences) > uscore:
-        return qidx_mine[np.where(confidences > uscore)]
-    return qidx_mine[np.argmax(confidences)][np.newaxis]
+    return qidx_mine, confidences
+
+
+def solve(board,
+          mines_remain: int = None,
+          consider_mines_th: int = 5,
+          guess_edge_weight: float = 2.0):
+    logger = logging.getLogger(__name__ + '.solve')
+    if np.all(board == CID['q']):
+        logger.info('Performing first step random guess')
+        randbloc = np.unravel_index(np.random.randint(board.size), board.shape)
+        logger.info('Choosing bloc=%s', randbloc)
+        return np.concatenate((randbloc, [0]))[np.newaxis]
+    if np.all(board != CID['q']):
+        logger.warning('No uncovered cells found. Why isn\'t the game ended?')
+        return np.array([])
+
+    try:
+        qidx_mine, confidences = solve_board(board, mines_remain,
+                                             consider_mines_th)
+        uscore = 1.0 - 1e-6
+        if np.max(confidences) > uscore:
+            return qidx_mine[np.nonzero(confidences > uscore)]
+        if not np.allclose(np.max(confidences), 0.0):
+            return qidx_mine[np.argmax(confidences)][np.newaxis]
+        # confidence == [0.0, 0.0, ...], mines should be [False, False, ...]
+        assert not np.any(qidx_mine[:, 2]), qidx_mine
+        logger.info('Confidences are all zero; failing back to random guess')
+        rand_bloc = guess(board, qidx_mine[:, :2], guess_edge_weight)
+        logger.info('Choosing: bloc=%s, mine_under=0', rand_bloc)
+        return np.concatenate((rand_bloc, [0]))[np.newaxis]
+    except NoSolutionError:
+        logger.warning('NoSolutionError')
+        logger.info('Falling back to random guess')
+        # guess edges with more probability
+        all_blocs = np.stack(np.nonzero(board == CID['q']), axis=1)
+        rand_bloc = guess(board, all_blocs, guess_edge_weight)
+        # if guess 1 it ends up mistaken but found after several steps
+        rand_mine = 0
+        logger.info('Choosing: bloc=%s, mine_under=%s', rand_bloc, rand_mine)
+        return np.concatenate((rand_bloc, [rand_mine]))[np.newaxis]
 
 
 def _main():
