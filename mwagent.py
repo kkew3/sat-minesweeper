b7a377ed35d5689cd8e7bfc5880bdec7b185ad09
kkew3 <kps6326@hotmail.com>
2023-05-31 17:31:04 +0800

    Used 'NatChrfBMouseClicker'.

--- mwagent.py
+++ mwagent.py
@@ -96,8 +96,8 @@ def main():
                 for i in range(1, len(sct.monitors))]
         # leave `enable_mr_detect` False intentionally
         bd = vb.BoardDetector.new(scrs)
-        mc = planner.NatChrfLBMouseClicker(sct.monitors[bd.mon_id], bd.dpr, bd,
-                                        sct)
+        mc = planner.NatChrfBMouseClicker(sct.monitors[bd.mon_id], bd.dpr, bd,
+                                          sct)
         pl = planner.GreedyChordActionPlanner(0.0, mc, sct)
         si = vb.StageIdentifier()
 
